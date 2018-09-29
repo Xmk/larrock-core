bdb884d3e1f08e79756c09eea2b1482496ff3329
Chiffa (Paul) <i@ars-team.ru>
2018-09-29 18:41:18 +0500

    fix larrock:renamePublicDirectory configs dir

--- src/Commands/LarrockRenamePublicDirectoryCommand.php
+++ src/Commands/LarrockRenamePublicDirectoryCommand.php
@@ -32,8 +32,8 @@ class LarrockRenamePublicDirectoryCommand extends Command
         if (! \File::exists(base_path('public_html/index.php'))) {
             \File::copyDirectory(base_path('public'), base_path('public_html'));
             \File::deleteDirectory(base_path('public'));
-            $dir = str_replace('/Commands', '', __DIR__);
-            \File::copy($dir.'/../configVendor/larrock-index-public_html_php', base_path('public_html/index.php'));
+            $dir = dirname(__FILE__, 3).DIRECTORY_SEPARATOR.'configVendor'.DIRECTORY_SEPARATOR;
+            \File::copy($dir.'larrock-index-public_html_php', base_path('public_html/index.php'));
             $this->info('Directory and index.php successfully updated');
         } else {
             $this->info('The command is not required, the directory has already changed.');
